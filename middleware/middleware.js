328cd51ab212987e493a202ab689f6be620b3ce1
Anoruo Benjamin <82318783+Benjamin4041@users.noreply.github.com>
2025-01-28 17:12:46 +0000

    just made userAuth middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,11 +1,28 @@
 const JWT = require("jsonwebtoken");
+
 async function userAuth(req, res, next) {
     try {
         const jwtToken = req.params.token
-        let verified = JWT.verify(jwtToken, process.env.JWT_password)
-        
+        let verified = await JWT.verify(jwtToken, process.env.JWT_password)
+        let userDetails = await User.findOne({verified.email})
+
+        if(!verified){
+            return res.send({message:'jwt expired or Deactevated',boolean:false})
+        }
+
+        if(!userDetails){
+            return res.send({message:'User does not exsist',boolean:false})
+        }
+        req.user = verified
+        return next()
     } catch (error) {
 
         return res.send({ message: error.message })
     }
-} 
+}
+
+
+
+module.exports = {
+    userAuth
+}
